008fbb99d580dfac4c1389123abb7edf344a2a26
mohammadmu1 <M_Mustafa9@outlook.com>
2023-12-15 01:10:32 +0300

    compose-all-services

---
+++ confidserver/src/test/java/com/example/confidserver/ConfidserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.confidserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfidserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
